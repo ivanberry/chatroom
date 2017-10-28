76d958b4ff09eeb8fd76896307c191c1c26b6a76
tab_blee <tabvim.dev@gmail>
2017-10-28 19:31:53 +0800

    (.all) Add client Chat class

--- public/javascript/chat.js
+++ public/javascript/chat.js
@@ -1,17 +1,19 @@
-let Chat = function (socket) {
+export default class Chat {
+  constructor(socket) {
     this.socket = socket;
-}
+  }
 
-Chat.prototype.sendMessage = (room, text) => {
+  sendMessage(room, text) {
     let message = {
-        room: room,
-        text: text,
+      room: room,
+      text: text
     };
-    this.socket.emit('message', message);
-};
+    this.socket.emit("message", message);
+  }
 
-Chat.prototype.changeRoom = (room) => {
-    this.socket.emit('join', {
-        newRoom: room
+  changeRoom(room) {
+    this.socket.emit("join", {
+      newRoom: room
     });
-};
+  }
+}
