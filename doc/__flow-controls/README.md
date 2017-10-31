7278080c70bd2cd38e3f70c19f9a02f6c67fb8eb
ivanberry <tabvim.dev@gmail.com>
2017-11-01 06:22:10 +0800

    (.location) :+1: Learn serial flow control

--- doc/__flow-controls/README.md
+++ doc/__flow-controls/README.md
@@ -8,3 +8,6 @@
 
 In order to execute a number of asynchronous tasks in sequece, you could use callbacks. but if you have a significant number of tasks, you should organize them, or you'll end up with messy code due to excessive callback nesting.
 
+#### Notice path
+The default execute path is `process.cwd()`, You may need `path.resolve(__dirname, _path)`, the `__dirname` always give the current parent path the file your executed
+
--- doc/__flow-controls/serise-controls.js
+++ doc/__flow-controls/serise-controls.js
@@ -5,15 +5,48 @@ path = require('path'),
 configFilename = './rss_feeds.txt',
 ROOT = __dirname;
 
-let tasks = [checkForRSSFile/*, readRSSFile, downloadRSSFeed, parseRSSFeed */];
+let tasks = [checkForRSSFile, readRSSFile, downloadRSSFeed, parseRSSFeed ];
 
 function checkForRSSFile() {
-  fs.stat(configFilename, (status) => {
-    if (!status) {
+  let _file_path = path.resolve(ROOT, configFilename);
+  fs.stat(_file_path, (err, stat) => {
+    if (err) {
       return next(new Error('Miss RSS file: ' + configFilename));
     }
-    next(null, configFilename);
+    next(null, _file_path);
   });
+}
+
+function readRSSFile(configFilename) {
+  fs.readFile(configFilename, (err, feedList) => {
+    if (err) return next(err);
+    feedList = feedList.toString().replace(/^\s+|\s+$/g, '');
+    // let random = Math.floor(Math.random() * feedList.length);
+    // next(null, feedList[random]);
+    next(null, feedList);
+  })
+}
+
+function downloadRSSFeed(feedUrl) {
+  request(feedUrl, (err, res, body) => {
+    if (err) return next(err);
+    if (res.statusCode !== 200) {
+      return next(new Error('Abnormal response status code'))
+    }
+    next(null, body);
+  });
+}
+
+function parseRSSFeed(rss) {
+  let handler = new htmlparser.RssHandler();
+  let parser = new htmlparser.Parser(handler);
+  parser.parseComplete(rss);
+
+  if (!handler.dom.items.length) return next(new Error('No RSS items found'))
+  let item = handler.dom.items.shift();
+  console.log('====================================');
+  console.log(item.title, item.link);
+  console.log('====================================');
 }
 
 function next(err, result) {
@@ -24,4 +57,4 @@ function next(err, result) {
   }
 }
 
-checkForRSSFile();
+next();
