208295604ead606b54493ec7aedcefc236ac59c2
ivanberry <tabvim.dev@gmail.com>
2017-11-07 07:06:31 +0800

    (.localtion) :cn: connect middleware

---
+++ doc/connect/server.js
@@ -0,0 +1,14 @@
+const connect = require("connect");
+const cookieParse = require("cookie-parser");
+const http = require("http");
+const app = connect();
+
+app.use(cookieParse("luna")).use((req, res) => {
+  console.log(req.cookies);
+  console.log(req.signedCookies);
+  res.setHeader('Set-Cookie', 'foo=bar');
+  res.setHeader('Set-Cookie', 'tobi=ferret; ExPires=Tue, 01 Jun 2020 10:10:10 GMT');
+  res.end("hello\n");
+});
+
+http.createServer(app).listen(9008);
