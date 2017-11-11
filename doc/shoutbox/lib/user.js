92aaca28ca8a18100faf8bf35135b948a0603373
ivanberry <tabvim.dev@gmail.com>
2017-11-12 06:15:34 +0800

    (.location) :two_hearts: Update User ulti method

--- doc/shoutbox/lib/user.js
+++ doc/shoutbox/lib/user.js
@@ -5,9 +5,41 @@ const db = redis.createClient();
 
 class User {
 	constructor(obj) {
-    let { id, pass} = obj;
+		let { id, pass, name, age} = obj;
+		this.name = name;
     this.id = id;
-    this.pass = pass;
+		this.pass = pass;
+		this.age = age;
+	}
+
+	static getByName (name, fn) {
+		User.getId(name, (err, id) => {
+			if (err) return fn(err);
+			User.get(id, fn);
+		});
+	}
+
+	static getId(name, id) {
+		db.get(`user:id:${name}`, fn);
+	}
+
+	static get(id, fn) {
+		db.hgetall(`user:${id}`, (err, user) => {
+			if (err) return fn(err);
+			fn(null, new User(user));
+		});
+	}
+
+	static authenticate  (name, pass, fn) {
+		User.getByName(name, (err, user) => {
+			if (err) return fn(err);
+			if (!user.id) return fn();
+			bcrypt.hash(pass, user.salt, (err, hash) => {
+				if (err) return fn(err);
+				if (hash === user.pass) return fn(null, user);
+				fn();
+			})
+		})
 	}
 
 	save(fn) {
@@ -51,16 +83,3 @@ class User {
 }
 
 module.exports = User;
-
-let tobi = new User({
-	name: 'Tobi',
-	pass: 'im a ferrt',
-	age: 1
-});
-
-console.log(tobi);
-
-tobi.save(err => {
-	if (err) throw err;
-	console.log(`user id is ${tobi.id}`);
-});
